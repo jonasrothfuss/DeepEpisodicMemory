390a86154f3712cffe157140750eb14f6fd6342c
fabio.ferreira@student.kit.edu <fabioferreira at mailbox dot org>
2017-02-15 23:54:52 +0100

    Gradient Difference Loss

--- models/train_model.py
+++ models/train_model.py
@@ -8,15 +8,48 @@ from tensorflow.python.platform import flags
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-ENCODER_LENGTH = 5 #TODO: define sequence length as flag
+ENCODER_LENGTH = 5  # TODO: define sequence length as flag
 DECODER_FUTURE_LENGTH = 5
 DECODER_RECONST_LENGTH = 5
-LOSS_FUNCTIONS = ['mse']
+LOSS_FUNCTIONS = ['mse', 'gdl']
 
 FLAGS = flags.FLAGS
 
 flags.DEFINE_integer('num_iterations', 100000, 'number of training iterations.')
-flags.DEFINE_string('loss_function', 'mse', 'loss function to minimize')
+flags.DEFINE_string('loss_function', 'gdl', 'loss function to minimize')
+
+
+def gradient_difference_loss(true, pred, alpha=2.0):
+  """description here"""
+  tf.assert_equal(tf.shape(true), tf.shape(pred))
+  # vertical
+  true_pred_diff_vert = tf.pow(tf.abs(difference_gradient(true, vertical=True) - difference_gradient(pred, vertical=True)), alpha)
+  # horizontal
+  true_pred_diff_hor = tf.pow(tf.abs(difference_gradient(true, vertical=False) - difference_gradient(pred, vertical=False)), alpha)
+  # normalization over all dimensions
+  return tf.reduce_sum(true_pred_diff_vert) + tf.reduce_sum(true_pred_diff_hor) / tf.to_float(2*tf.size(pred))
+
+
+
+def difference_gradient(image, vertical=True):
+  # two dimensional tensor
+  # rank = ndim in numpy
+  #tf.assert_rank(tf.rank(image), 4)
+
+  # careful! begin is zero-based; size is one-based
+  if vertical:
+    begin0 = [0, 0, 0]
+    begin1 = [1, 0, 0]
+    size = [tf.shape(image)[1] - 1, tf.shape(image)[2], tf.shape(image)[3]]
+  else: # horizontal
+    begin0 = [0, 0, 0]
+    begin1 = [0, 1, 0]
+    size = [tf.shape(image)[1], tf.shape(image)[2] - 1, tf.shape(image)[3]]
+
+  slice0 = tf.slice(image[0, :, :, :], begin0, size)
+  slice1 = tf.slice(image[0, :, :, :], begin1, size)
+  return tf.abs(tf.sub(slice0, slice1))
+
 
 def mean_squared_error(true, pred):
   """L2 distance between tensors true and pred.
@@ -28,6 +61,7 @@ def mean_squared_error(true, pred):
   """
   return tf.reduce_sum(tf.square(true - pred)) / tf.to_float(tf.size(pred))
 
+
 def peak_signal_to_noise_ratio(true, pred):
   """Image quality metric based on maximal signal power vs. power of the noise.
   Args:
@@ -37,6 +71,7 @@ def peak_signal_to_noise_ratio(true, pred):
     peak signal to noise ratio (PSNR)
   """
   return 10.0 * tf.log(1.0 / mean_squared_error(true, pred)) / tf.log(10.0)
+
 
 def decoder_loss(frames_gen, frames_original, loss_fun):
   """Sum of parwise loss between frames of frames_gen and frames_original
@@ -51,8 +86,12 @@ def decoder_loss(frames_gen, frames_original, loss_fun):
   loss = 0.0
   if loss_fun == 'mse':
     for i in range(len(frames_gen)):
-      loss += mean_squared_error(frames_original[:,i,:,:,:], frames_gen[i])
+      loss += mean_squared_error(frames_original[:, i, :, :, :], frames_gen[i])
+  if loss_fun == 'gdl':
+    for i in range(len(frames_gen)):
+      loss += gradient_difference_loss(frames_original[:, i, :, :, :], frames_gen[i])
   return loss
+
 
 def decoder_psnr(frames_gen, frames_original, loss_fun):
   """Sum of peak_signal_to_noise_ratio loss between frames of frames_gen and frames_original
@@ -65,14 +104,16 @@ def decoder_psnr(frames_gen, frames_original, loss_fun):
   """
   psnr = 0.0
   for i in range(len(frames_gen)):
-    psnr += peak_signal_to_noise_ratio(frames_original[:,i,:,:,:], frames_gen[i])
+    psnr += peak_signal_to_noise_ratio(frames_original[:, i, :, :, :], frames_gen[i])
   return psnr
 
+
 def composite_loss(original_frames, frames_pred, frames_reconst, loss_fun='mse',
-                   encoder_length=ENCODER_LENGTH, decoder_future_length=DECODER_FUTURE_LENGTH, decoder_reconst_length=DECODER_RECONST_LENGTH):
-  assert encoder_length<=decoder_reconst_length
+                   encoder_length=ENCODER_LENGTH, decoder_future_length=DECODER_FUTURE_LENGTH,
+                   decoder_reconst_length=DECODER_RECONST_LENGTH):
+  assert encoder_length <= decoder_reconst_length
   assert loss_fun in LOSS_FUNCTIONS
-  frames_original_future = original_frames[:, (encoder_length):(encoder_length + decoder_future_length ), :, :, :]
+  frames_original_future = original_frames[:, (encoder_length):(encoder_length + decoder_future_length), :, :, :]
   frames_original_reconst = original_frames[:, (encoder_length - decoder_reconst_length):encoder_length, :, :, :]
   pred_loss = decoder_loss(frames_pred, frames_original_future, loss_fun)
   reconst_loss = decoder_loss(frames_reconst, frames_original_reconst, loss_fun)
@@ -80,32 +121,32 @@ def composite_loss(original_frames, frames_pred, frames_reconst, loss_fun='mse',
 
 
 def main(unused_argv):
+  # mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-  #mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+  x = tf.placeholder(tf.float32, shape=[None, None, 128, 128, 1])  # 128x128 images
 
-  x = tf.placeholder(tf.float32, shape=[None, None, 128, 128, 1]) # 128x128 images
+  frames_pred, frames_reconst = model.composite_model(x, ENCODER_LENGTH, DECODER_FUTURE_LENGTH, DECODER_RECONST_LENGTH,
+                                                      num_channels=1)
 
-  frames_pred, frames_reconst = model.composite_model(x, ENCODER_LENGTH, DECODER_FUTURE_LENGTH, DECODER_RECONST_LENGTH, num_channels=1)
+  # Loss Function
+  loss = composite_loss(x, frames_pred, frames_reconst, loss_fun=FLAGS.loss_function)
 
-  #Mean Squared Error - Loss Function
-  loss = composite_loss(x, frames_pred, frames_reconst)
-
-  #choose optimizer
+  # choose optimizer
   train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
   sess = tf.InteractiveSession()
 
-  #start session
+  # start session
   sess.run(tf.global_variables_initializer())
 
-
-  #train
+  # train
   for i in range(FLAGS.num_iterations):
-    batch = np.random.rand(50,10,128,128,1)
-    assert(batch.shape[2] >= (ENCODER_LENGTH + DECODER_FUTURE_LENGTH))
+    batch = np.random.rand(50, 10, 128, 128, 1)
+    assert (batch.shape[2] >= (ENCODER_LENGTH + DECODER_FUTURE_LENGTH))
     train_step.run(feed_dict={x: batch})
     tf.logging.info(str(i))
 
 
 if __name__ == '__main__':
   app.run()
+

2bef23ee86df1430378de20ac93c6d86d0253735
Jonas Rothfuss <jonas.rothfuss@gmx.de>
2017-06-30 17:13:27 +0200

    better documentation sim matrix calculation

--- data_postp/similarity_computations.py
+++ data_postp/similarity_computations.py
@@ -25,7 +25,7 @@ NUM_CORES = multiprocessing.cpu_count()
 # /localhome/rothfuss/training/04-27-17_20-40/valid_run/metadata_and_hidden_rep_df_05-04-17_09-06-48.pickle
 #PICKLE_FILE_DEFAULT = '/localhome/rothfuss/training/06-09-17_16-10/valid_run/metadata_and_hidden_rep_df_06-22-17_18-41-25.pickle'
 #PICKLE_FILE_DEFAULT = '/common/homes/students/rothfuss/metadata_and_hidden_rep_df_06-22-17_18-41-25.pickle'
-PICKLE_FILE_DEFAULT = '/common/homes/students/rothfuss/Documents/training/06-26-17_14_32_noise1/valid_run/metadata_and_hidden_rep_df_06-29-17_18-55-26.pickle'
+PICKLE_FILE_DEFAULT = '/common/homes/students/rothfuss/Documents/training/06-26-17_14_32_noise1/valid_run/metadata_and_hidden_rep_df_06-30-17_17-03-42.pickle'
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string('pickle_file', PICKLE_FILE_DEFAULT, 'path of panda dataframe pickle file ')
@@ -100,7 +100,6 @@ def compute_cosine_similarity(vector_a, vector_b):
 def square_rooted(x):
     return round(sqrt(sum([a * a for a in x])), 3)
 
-
 def visualize_hidden_representations(pickle_hidden_representations):
     # shatter 1000x8x8x16 to 1000x1024 dimensions
     labels = list(pickle_hidden_representations['shape'])
@@ -123,7 +122,6 @@ def visualize_hidden_representations(pickle_hidden_representations):
 
     plt.show()
 
-
 def mean_vector(vector_list):
     """Computes the mean vector from a list of ndarrays
     :return mean_vector - ndarray
@@ -137,7 +135,6 @@ def mean_vector(vector_list):
     assert type(mean_vector) is np.ndarray
     # return mean_vector
 
-
 def df_col_to_matrix(panda_col):
     """Converts a pandas dataframe column wherin each element in an ndarray into a 2D Matrix
     :return ndarray (2D)
@@ -145,7 +142,6 @@ def df_col_to_matrix(panda_col):
     """
     panda_col = panda_col.map(lambda x: x.flatten())
     return np.vstack(panda_col)
-
 
 def principal_components(df_col, n_components=2):
     """Performs a Principal Component Analysis (fit to the data) and then returns the returns the data transformed corresponding to the n_components
@@ -158,7 +154,6 @@ def principal_components(df_col, n_components=2):
     pca = sklearn.decomposition.PCA(n_components)
     X = df_col_to_matrix(df_col)  # reshape dataframe column consisting of ndarrays as 2D matrix
     return pca.fit_transform(X)
-
 
 def distance_classifier(df):
     """Handmade classifier that computes the center of each class and then assigns each datapoint the class with the closest centerpoint
@@ -182,7 +177,6 @@ def distance_classifier(df):
     print('Accuracy with euclidian distance:', df['eucl_correct'].mean())
     print('Accuracy with cosine distance:', df['cos_correct'].mean())
     return df
-
 
 def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
     """
@@ -258,7 +252,6 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=1, tr
             transform=ax.transAxes)
 
     return plt
-
 
 def svm_fit_and_score(hidden_representations_pickle, class_column="shape", type="linear", CV=False, plot=False):
     """
@@ -475,42 +468,53 @@ def avg_distance(df, similarity_type='cos'):
                 out_class_dist_array.append(distance)
     return np.mean(same_class_dist_array), np.mean(out_class_dist_array)
 
-
 def similarity_matrix(df, df_label_col, similarity_type='cos', vector_type = "", plot_options=((64, 64), 15, 15)):
-    assert 'hidden_repr' in list(df) and df_label_col in list(df)
-    assert similarity_type in ['cos'] #euclidean no longer supported
-    labels = list(sorted(set(df[df_label_col])))
-    n = len(labels)
-    sim_matrix = np.zeros([n, n])
-    for i, j in itertools.product(range(n), range(n)):
-      if i <= j: # since similarity matrix is mirrored along the diagonal, only compute one half
-        print(i, j, labels[i], labels[j])
-        vectors1 = np.asmatrix([v.flatten() for v in list(df[df[df_label_col] == labels[i]]['hidden_repr'])])
-        vectors2 = np.asmatrix([v.flatten() for v in list(df[df[df_label_col] == labels[j]]['hidden_repr'])])
-        kernel_matrix = sklearn.metrics.pairwise.cosine_similarity(vectors1, vectors2)
-        sim_matrix[i, j] = np.mean(kernel_matrix)
+  """
+  Computes a matrix the mean pairwise cosine similarities of the hidden vectors belongining to different classes
 
-    #mirror the second half
-    for i, j in itertools.product(range(n), range(n)):
-      if i > j:
-        sim_matrix[i, j] = sim_matrix[j, i]
+  :param df: pandas dataframe that contains the hidden vectors in a column called 'hidden_repr' and the corresponding class labels
+  :param df_label_col: name of the column with the class labels
+  :param similarity_type: denotes the similarity metric that is used (so far only 'cos' is supported)
+  :param vector_type: optional string that is included in the name of the dumped files
+  :param plot_options: list of settings for matplotlib and seaborn. First list element specifies figure size as a
+  tuple e.g. 64x64. Second list element specifies font_scale for seaborn as a single integer, e.g. 15. Third list
+  element specifies annotation font size as a single integer, e.g. 15)
+  :return: inter class cosine similarity matrix (ndarray with shape (n_classes, n_classes))
+  """
+  assert 'hidden_repr' in list(df) and df_label_col in list(df)
+  assert similarity_type in ['cos'] #euclidean no longer supported
+  labels = list(sorted(set(df[df_label_col])))
+  n = len(labels)
+  sim_matrix = np.zeros([n, n])
+  for i, j in itertools.product(range(n), range(n)):
+    if i <= j: # since similarity matrix is mirrored along the diagonal, only compute one half
+      print(i, j, labels[i], labels[j])
+      vectors1 = np.asmatrix([v.flatten() for v in list(df[df[df_label_col] == labels[i]]['hidden_repr'])])
+      vectors2 = np.asmatrix([v.flatten() for v in list(df[df[df_label_col] == labels[j]]['hidden_repr'])])
+      kernel_matrix = sklearn.metrics.pairwise.cosine_similarity(vectors1, vectors2)
+      sim_matrix[i, j] = np.mean(kernel_matrix)
 
-    df_cm = pd.DataFrame(sim_matrix, index=labels, columns=labels)
-    #print(df_cm)
-    df_cm.to_pickle(os.path.join(os.path.dirname(FLAGS.pickle_file), 'sim_matrix_' + similarity_type + '_' + vector_type + '.pickle'))
-    plt.figure(figsize=plot_options[0])
-    sn.set(font_scale=plot_options[1])
-    ax = sn.heatmap(df_cm, annot=True, annot_kws={"size": plot_options[2]})
-    if n > 5:
-        # rotate x-axis labels
-        for item in ax.get_xticklabels():
-            item.set_rotation(90)
-    heatmap_file_name = os.path.join(os.path.dirname(FLAGS.pickle_file),
-                                     'sim_matrix_font_large_' + df_label_col + '_' + similarity_type + '_' + vector_type + '.png')
-    plt.savefig(heatmap_file_name, dpi=100)
-    print('Dumped Heatmap to:', heatmap_file_name)
-    plt.show()
-    return sim_matrix
+  #mirror the second half
+  for i, j in itertools.product(range(n), range(n)):
+    if i > j:
+      sim_matrix[i, j] = sim_matrix[j, i]
+
+  df_cm = pd.DataFrame(sim_matrix, index=labels, columns=labels)
+  #print(df_cm)
+  df_cm.to_pickle(os.path.join(os.path.dirname(FLAGS.pickle_file), 'sim_matrix_' + similarity_type + '_' + vector_type + '.pickle'))
+  plt.figure(figsize=plot_options[0])
+  sn.set(font_scale=plot_options[1])
+  ax = sn.heatmap(df_cm, annot=True, annot_kws={"size": plot_options[2]})
+  if n > 5:
+      # rotate x-axis labels
+      for item in ax.get_xticklabels():
+          item.set_rotation(90)
+  heatmap_file_name = os.path.join(os.path.dirname(FLAGS.pickle_file),
+                                   'sim_matrix_font_large_' + df_label_col + '_' + similarity_type + '_' + vector_type + '.png')
+  plt.savefig(heatmap_file_name, dpi=100)
+  print('Dumped Heatmap to:', heatmap_file_name)
+  plt.show()
+  return sim_matrix
 
 def plot_similarity_shape_motion_matrix(df):
     """This function computes the large square confusion matrix with the dimensions being of shape (num_shapes * num_directions) and plots it using matplotlib"""
@@ -551,7 +555,6 @@ def plot_similarity_shape_motion_matrix(df):
     heatmap_file_name = os.path.join(os.path.dirname(FLAGS.pickle_file), 'large_sim_matrix.png')
     plt.savefig(heatmap_file_name, dpi=100)
     sn.plt.show()
-
 
 def compute_small_confusion_matrix(df, shape1, shape2):
     """Used to compute a square confusion matrix with the dimensions being of shape
@@ -603,7 +606,6 @@ def compute_small_confusion_matrix(df, shape1, shape2):
                 str(shape1) + '_' + str(shape2) + '_' + str(direction1) + '_' + str(direction2))
 
     return confusion_matrix
-
 
 def classifier_analysis(df, class_column='shape'):
     svm_linear_accuracy = svm_fit_and_score(df, class_column=class_column, type='linear')

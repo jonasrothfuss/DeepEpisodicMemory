7bbd5bdcb1d81c1467de75ca8c21fbbf74e3aa80
Fabio Ferreira <f4bio.ferreira@gmail.com>
2017-08-23 17:08:32 +0200

    video augmentation with brightness and saturation
    Signed-off-by: Fabio Ferreira <f4bio.ferreira@gmail.com>

--- data_prep/augmentation.py
+++ data_prep/augmentation.py
@@ -2,11 +2,11 @@ import os, subprocess
 import utils.io_handler as io
 import itertools
 
-AVI_SOURCE_DIR = '/data/rothfuss/data/20bn-something/augmentation_test'
-TARGET_DIR = '/data/rothfuss/data/20bn-something/augmentation_test/augmented'
+AVI_SOURCE_DIR = '/data/rothfuss/data/ArmarExperiences/videos/memory'
+TARGET_DIR = '/data/rothfuss/data/ArmarExperiences/videos/memory_augmented'
 
 GAMMA_VALS = [0.7, 1.0]
-BRIGHTNESS_VALS = [-0.2, 0.0, 0.2]
+BRIGHTNESS_VALS = [-0.1, 0.0, 0.2]
 SATURATION = [0.6, 1.0]
 
 
@@ -16,11 +16,14 @@ def augment_video(video_path, target_dir, gamma, brightness, saturation, i):
   video_id = os.path.basename(video_path).split('.')[0]
   new_video_path = os.path.join(target_dir, video_id + '_%i.avi'%i)
   ffmpeg_str = "/common/homes/students/rothfuss/Documents/ffmpeg/bin/ffmpeg -i %s -vf eq=gamma=%.1f:brightness=%.1f:saturation=%.1f -c:a copy %s"%(video_path, gamma, brightness, saturation, new_video_path)
-  #print(ffmpeg_str)
-  cp_str = "cp %s %s" % (video_path, new_video_path)
-  print(cp_str)
+  print(ffmpeg_str)
   subprocess.Popen(ffmpeg_str, shell=True)
 
-for i, (gamma, brightness, saturation) in enumerate(itertools.product(GAMMA_VALS, BRIGHTNESS_VALS, SATURATION)):
-  for file in avi_files:
-    augment_video(file, TARGET_DIR, gamma, brightness, saturation, i)
+
+for file in avi_files:
+  for i, (gamma, brightness, saturation) in enumerate(itertools.product(GAMMA_VALS, BRIGHTNESS_VALS, SATURATION)):
+    try:
+      augment_video(file, TARGET_DIR, gamma, brightness, saturation, i)
+      print("%i: "%i +  "Sucessfully augmented " + file)
+    except Exception as e:
+      print("%i: "%i + "Could not augment " + file + " --- " + str(e))

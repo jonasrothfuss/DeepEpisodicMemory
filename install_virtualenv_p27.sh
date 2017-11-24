fb226a5c0b201566b624a4deeb9e54cd75bc0426
Fábio Ferreira <f4bio.ferreira@gmail.com>
2017-11-24 18:31:34 +0100

    Update install_virtualenv_p27.sh

--- install_virtualenv_p27.sh
+++ install_virtualenv_p27.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-virtualenv ~/virtual_envs/tf-gpu
+pip install --user --upgrade virtualenv
+mkdir ~/virtual_envs
+virtualenv -p python --system-site-packages ~/virtual_envs/tf-gpu
 source ~/virtual_envs/tf-gpu/bin/activate
 pip install -r ~/DeepEpisodicMemory/requirements.txt
-export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.1-cp34-cp34m-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.1-cp27-none-linux_x86_64.whl
+pip install --user --upgrade $TF_BINARY_URL

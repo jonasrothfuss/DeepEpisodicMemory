440d948f86653edf916c9adc8d3f9af79da5a6f5
Fábio Ferreira <f4bio.ferreira@gmail.com>
2017-11-24 18:30:50 +0100

    Update install_virtualenv_p34.sh

--- install_virtualenv_p34.sh
+++ install_virtualenv_p34.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-virtualenv ~/virtual_envs/tf-gpu
+pip install --user --upgrade virtualenv
+mkdir ~/virtual_envs
+virtualenv -p python3 --system-site-packages ~/virtual_envs/tf-gpu
 source ~/virtual_envs/tf-gpu/bin/activate
 pip3 install -r ~/DeepEpisodicMemory/requirements.txt
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.1-cp34-cp34m-linux_x86_64.whl
-pip3 install --upgrade $TF_BINARY_URL
+pip3 install --user --upgrade $TF_BINARY_URL

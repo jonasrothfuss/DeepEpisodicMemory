6dd9050c03b390c3d5bc835b00414df87baa54bd
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2017-12-18 17:00:51 +0100

    python3 to python27 in script

--- install_virtualenv_tf_p27.sh
+++ install_virtualenv_tf_p27.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 pip install --user --upgrade virtualenv
-virtualenv -p python3 --system-site-packages ~/virtual_envs/tf
+virtualenv -p python --system-site-packages ~/virtual_envs/tf
 source ~/virtual_envs/tf/bin/activate
-pip3 install -r ./requirements.txt
+pip install -r ./requirements.txt
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.1-cp27-none-linux_x86_64.whl
-pip3 install --user --upgrade $TF_BINARY_URL
+pip install --user --upgrade $TF_BINARY_URL

35132b7c590809ff3a7240c41051b79529cbef89
ferreirafabio <f4bio.ferreira@gmail.com>
2018-01-31 15:36:19 +0100

    fixed nan error, refactoring

--- data_postp/scores.py
+++ data_postp/scores.py
@@ -6,81 +6,57 @@ from tensorflow.python.platform import flags
 FLAGS = flags.FLAGS
 
 
-def compute_mean_average_precision(df, match_matrix=False, column_keyword="pred_class"):
-  assert df is not None
+def compute_mean_average_precision(df_base, df_query, n_closest_matches=5):
   """
-  either provide a pandas dataframe with shape (n, m) with n being the number of queries, m being the number of columns
-  or provide a matrix with true/false values with the same shape as above representing the matching between true class labels
-  and predicted class labels.
+  This function computes the mean average precision (MAP) for a set of queries specified by df_query. The average precision
+  scores for each query are hereby computed based on the provided base instances specified in df_base. For each query,
+  the nearest neighbor instances within the base are determined and used to compute the precision.
+  :param df_base: the dataframe to be queried, must contain a 'hidden_repr' column that constitutes the hidden_representation vector
+  :param df_query: the dataframe from which to query, must contain a 'hidden_repr' column
+  :param n_closest_matches: number of closest matches to the query that goes into the precision score
+  :return: a scalar value representing the MAP
   """
-  if not match_matrix:
-    df_pred_classes = df.filter(like=column_keyword)
-    n_relevant_documents = len(df_pred_classes.columns)
-    matches = df_pred_classes.isin(df.true_class).as_matrix()
-  else:
-    n_relevant_documents = np.shape(df)[1]
-    matches = df
+  assert not df_base.empty and not df_query.empty
+  df = get_query_matching_table(df_base=df_base, df_query=df_query, n_closest_matches=n_closest_matches)
+
+  df_pred_classes = df.filter(like="pred_class")
+  n_relevant_documents = len(df_pred_classes.columns)
+  matches = df_pred_classes.isin(df.true_class).as_matrix()
 
   P = np.zeros(shape=matches.shape)
-  for k in range(n_relevant_documents):
+  for k in range(1, n_relevant_documents):
     P[:, k] = np.mean(matches[:, :k], axis=1)
 
   return np.mean(np.multiply(P, matches))
 
 
-def get_y_true_y_pred(df, true_class_column="true_class", column_keyword="pred_class"):
-  assert df is not None
-  df_true_classes = df.filter(like=true_class_column)
-  df_pred_classes = df.filter(like=column_keyword)
-  all_classes = np.unique(pd.concat([df_true_classes, df_pred_classes], axis=1))
-
-  matches = df_pred_classes.isin(df.true_class)
-
-
-  df_y_true = pd.DataFrame(np.full((len(df.index), len(all_classes)), False), columns=all_classes)
-  for i, row in matches.iterrows():
-    if row.any():
-      """ get the true label, determine location and set to True """
-      label = df_true_classes.iloc[i].values
-      df_y_true.set_value(i, label, True)
-
-
-  df_y_pred = pd.DataFrame(np.full((len(df.index), len(all_classes)), False), columns=all_classes)
-  for i, row in df_pred_classes.iterrows():
-    for entry in row.values:
-      df_y_pred.set_value(i, entry, True)
-
-  return df_y_true.as_matrix(), df_y_pred.as_matrix(), all_classes
-
-
-
-def get_query_matching_table(df, df_query, class_column='shape', n_closest_matches=5, df_query_label="category", df_query_id="id"):
+def get_query_matching_table(df_base, df_query, class_column='category', n_closest_matches=5, df_true_label="true_class",
+                             df_pred_label="pred_class_", df_query_label="category", df_query_id="id"):
   """
   Yields a pandas dataframe in which each row contains the n_closest_matches as a result from querying the df for every single
   hidden representation in the df dataframe. In addition, every row contains the true label and the query id.
-  :param df: the df to be queried
+  :param df_base: the df to be queried
   :param df_query: the df from which to query
-  :return: pandas dataframe with columns ("id", "true_label", "match i pred_class" for i=1,...,n_closest_matches) and
+  :return: pandas dataframe with columns ("id", "true_label", "pred_class_i" for i=1,...,n_closest_matches) and
   number of rows equal to df_query rows
   """
-  assert df is not None and df_query is not None
-  assert 'hidden_repr' in df.columns and class_column in df.columns
+  assert df_base is not None and df_query is not None
+  assert 'hidden_repr' in df_base.columns and class_column in df_base.columns
   assert 'hidden_repr' in df_query.columns and df_query_label in df_query.columns and df_query_id in df_query.columns
 
-  columns = [["id{}".format(i), "pred_class{}".format(i)] for i in range(1, n_closest_matches + 1)]
+  columns = [[df_query_id + "{}".format(i), df_pred_label+"{}".format(i)] for i in range(1, n_closest_matches + 1)]
   columns = [e for entry in columns for e in entry] # flatten list in list
-  columns[:0] = ["id", "true_class"]
+  columns[:0] = [df_query_id, df_true_label]
 
   query_matching_df = pd.DataFrame(columns=columns)
-  query_matching_df.set_index('id', 'true_class')
+  query_matching_df.set_index(df_query_id, df_true_label)
 
   for hidden_repr, label, id in zip(df_query['hidden_repr'], df_query[df_query_label], df_query[df_query_id]):
-    closest_vectors = similarity_computations.find_closest_vectors(df, hidden_repr=hidden_repr, class_column=class_column,
-                                           n_closest_matches=n_closest_matches)
-
+    closest_vectors = similarity_computations.find_closest_vectors(df_base, hidden_repr=hidden_repr, class_column=class_column,
+                                                              n_closest_matches=n_closest_matches)
 
     matching_results = [[tpl[2], tpl[1]] for tpl in closest_vectors]
-    matching_results = sum(matching_results, [])  #flatten
+    matching_results = sum(matching_results, [])  # flatten
     matching_results[:0] = [id, label]
 
     row_data = dict(zip(columns, matching_results))
@@ -100,11 +76,11 @@ def main():
   train_df = df[msk]
   print("number of train samples: ", np.shape(train_df)[0])
 
-  df, df_val = similarity_computations.transform_vectors_with_inter_class_pca(train_df, test_df, class_column='category', n_components=50)
-  df = get_query_matching_table(df[:100], df_val[:100], class_column="category")
-  print(compute_mean_average_precision(df))
+  #df, df_val = similarity_computations.transform_vectors_with_inter_class_pca(train_df, test_df, class_column='category', n_components=50)
+  compute_mean_average_precision(train_df[:100], test_df[:100])
 
 
 
 if __name__ == "__main__":
   main()
+
